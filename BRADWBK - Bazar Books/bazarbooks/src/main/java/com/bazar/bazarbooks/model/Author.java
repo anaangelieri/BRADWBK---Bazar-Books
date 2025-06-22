d04d68fb8f0cb2e62a8e9241753999eebba3538b
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 13:04:14 -0300

    [ Fixes ] Author

--- BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/model/Author.java
+++ BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/model/Author.java
@@ -2,7 +2,11 @@ package com.bazar.bazarbooks.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
@@ -10,11 +14,13 @@ import jakarta.persistence.OneToMany;
 public class Author {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idAuthor;
     private String name;
     private String imageUrl;
     private String description;
     @OneToMany(mappedBy = "author")
+    @JsonManagedReference
     private List<Book> books;
 
     public int getIdAuthor() {
