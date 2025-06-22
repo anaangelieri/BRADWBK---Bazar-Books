164975ef369303ad2df3ca360a5ea8d45b5cc9f0
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 13:09:56 -0300

    [ Implementações ]  Author

---
+++ BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/controller/AuthorController.java
@@ -0,0 +1,63 @@
+package com.bazar.bazarbooks.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bazar.bazarbooks.model.Author;
+import com.bazar.bazarbooks.service.AuthorService;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable int id) {
+        Author author = authorService.getAuthorById(id);
+        if (author != null) {
+            return ResponseEntity.ok().body(author);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        Author created = authorService.putAuthor(author);
+        return ResponseEntity.ok(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthor(@PathVariable int id, @RequestBody Author author) {
+        boolean updated = authorService.updateAuthor(id, author);
+        if (updated) {
+            return ResponseEntity.ok().body(author);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAuthor(@PathVariable int id) {
+        boolean deleted = authorService.deleteAuthor(id);
+        if (deleted) {
+            return ResponseEntity.ok("Author successfully removed!");
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
