9fe49c398e750a508999e8a9bee9b295c4f658f5
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 13:31:56 -0300

    [ Fixes ] Address

--- BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/model/Address.java
+++ BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/model/Address.java
@@ -24,6 +24,7 @@ public class Address {
     private double longitude;
     @ManyToOne
     @JoinColumn(name = "idUser")
+    @JsonBackReference
     private User user;
 
     public int getIdAddress() {
