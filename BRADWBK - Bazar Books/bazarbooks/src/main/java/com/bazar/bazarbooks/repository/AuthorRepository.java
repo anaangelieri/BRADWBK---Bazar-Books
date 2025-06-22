8c83bb6b82712f524b7a5db6c7b1cccecedf86a6
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-22 13:07:45 -0300

    [ Implementações ] Author

---
+++ BRADWBK - Bazar Books/bazarbooks/src/main/java/com/bazar/bazarbooks/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.bazar.bazarbooks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bazar.bazarbooks.model.Author;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
